96ee92a8db4d0192154e0b89ca8c6dbbb216ec3f
Akila <akila271819@gmail.com>
2025-08-30 02:29:57 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import ClickCounter from "./components/Clickcounter";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div>
-      <ClickCounter />
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 space-y-6">
+      {/* With custom props */}
+      <Profile name="Akila" age={32} city="Chennai" />
+
+      {/* Without props → uses defaultProps */}
+      <Profile />
     </div>
   );
 }
